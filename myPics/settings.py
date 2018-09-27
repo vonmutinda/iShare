0a44d637f9c9d66765cbea092ad7ce1e02ac6b34
vonmutinda <maxwellwambua@gmail.com>
2018-09-27 19:18:01 +0300

    adding bootstrap and js

--- myPics/settings.py
+++ myPics/settings.py
@@ -25,6 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pics',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -106,3 +108,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
