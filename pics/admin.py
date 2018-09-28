a90cfdbfd09f9688c4a584f68027a1e32309e9af
vonmutinda <maxwellwambua@gmail.com>
2018-09-28 11:28:27 +0300

    Mutinda Model and Image Model

--- pics/admin.py
+++ pics/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Mutinda , Image
+
+
 
 # Register your models here.
+admin.site.register(Mutinda)
+admin.site.register(Image)
--- pics/models.py
+++ pics/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class Mutinda(models.Model):
+    name = models.CharField(max_length = 30)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length = 15 , blank = True)
+
+    def __str__(self):
+        return self.name
+
+
+    '''
+    Method for saving a new editor to the database
+    '''
+    def save_mutinda(self):
+        self.save()
+    
+
+    '''
+    Class method for deleting an editor
+    '''
+    def delete_mutinda(self):
+        self.delete()
+
+class Image(models.Model):
+    name = models.CharField(max_length = 30)
+    description = models.TextField()
+    user = models.ForeignKey(Mutinda)
+    image = models.ImageField(upload_to = 'articles/')
+
+
+
+
+    @classmethod
+    def search_by_title(cls,search_term):
+        articles = cls.objects.filter(title__icontains = search_term)
+        return articles
